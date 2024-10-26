ff151a39c52e8d7c3b9d864913144d76e6f472e1
DTapiaSanchez <d.tapia.sanchez@gmail.com>
2024-10-26 20:18:53 +0200

    mas modificaciones de direcciones relativas

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-YFIAD3TU.mjs";import"./chunk-MWOURRR6.mjs";import"./chunk-H262QN5A.mjs";import"./chunk-4LQOXGXI.mjs";import"./chunk-3GZLQVJH.mjs";import"./chunk-IZ6JN2LE.mjs";import"./chunk-3D55TWHC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-YPKNFMGZ.mjs";import"./chunk-MWOURRR6.mjs";import"./chunk-H262QN5A.mjs";import"./chunk-4LQOXGXI.mjs";import"./chunk-3GZLQVJH.mjs";import"./chunk-IZ6JN2LE.mjs";import"./chunk-3D55TWHC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
