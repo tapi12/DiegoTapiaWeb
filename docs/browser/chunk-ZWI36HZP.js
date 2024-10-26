748cf3d9209b4bf8545f9c49eac0f3f3ec9f106e
DTapiaSanchez <d.tapia.sanchez@gmail.com>
2024-10-26 18:03:46 +0200

    añadimos la carpeta docs para el despliegue Front-End

---
+++ docs/browser/chunk-ZWI36HZP.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-BEUPKLLM.js";import"./chunk-KHQEGF4B.js";import"./chunk-6CTV5HIS.js";export{a as TrailerVideoComponent};
---
+++ docs/server/chunk-QSKT3YYR.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-4LQOXGXI.mjs";import"./chunk-3GZLQVJH.mjs";import"./chunk-IZ6JN2LE.mjs";import"./chunk-3D55TWHC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as TrailerVideoComponent};
