96090103b52c916ffd6f70cf618bc8f217efd128
DTapiaSanchez <d.tapia.sanchez@gmail.com>
2024-10-27 21:18:07 +0100

    modificacion moviles finales

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WTQMLK2K.mjs";import"./chunk-ZMWNPSCC.mjs";import"./chunk-KQWQGLDF.mjs";import"./chunk-LMITAXYN.mjs";import"./chunk-IU2WTUBM.mjs";import"./chunk-OBIA7UST.mjs";import"./chunk-6PMJM2AS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-3P42DYNV.mjs";import"./chunk-ZMWNPSCC.mjs";import"./chunk-KQWQGLDF.mjs";import"./chunk-LMITAXYN.mjs";import"./chunk-IU2WTUBM.mjs";import"./chunk-OBIA7UST.mjs";import"./chunk-6PMJM2AS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
